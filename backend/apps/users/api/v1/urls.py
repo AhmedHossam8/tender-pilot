59267632d3c79ac7750a2d1f5f7f8a40fec76862
abdomonstasser <afoda6482@gmail.com>
2025-12-21 18:36:30 +0200

    modify urls  in app users

--- backend/apps/users/api/v1/urls.py
+++ backend/apps/users/api/v1/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-# from apps.users.views import UserListView, UserDetailView
+from rest_framework_simplejwt.views import TokenRefreshView
+from apps.users.views import RegisterAPIView, LoginAPIView , ProfileAPIView , UpdateReadApiView,AdminListAPIView,AdminUserDetailAPIView
 
 urlpatterns = [
-    # path('', UserListView.as_view(), name='user-list'),
-    # path('<int:pk>/', UserDetailView.as_view(), name='user-detail'),
+    path('register/',RegisterAPIView.as_view()),
+    path('login/',LoginAPIView.as_view()),
+    path('refresh/',TokenRefreshView.as_view()),
+    path('profile/',ProfileAPIView.as_view()),
+    path('me/',UpdateReadApiView.as_view()),
+    path('admin/userslist/',AdminListAPIView.as_view()),
+    path('admin/users/<int:pk>/',AdminUserDetailAPIView.as_view())
 ]
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -170,7 +170,8 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_TYPES': ('Bearer',)
+}
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
