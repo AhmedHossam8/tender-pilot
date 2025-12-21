22708be9ff72759ede62631e37e957041c7ad8fc
AhmedHossam8 <ahmedhossamemara8@gmail.com>
2025-12-21 16:40:33 +0200

    TP-26-finishing all APIs structure

--- backend/common/api/v1/urls.py
+++ backend/common/api/v1/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('ai/', include('ai_engine.api.v1.urls')),
-    # path('tenders/', include('tenders.api.v1.urls')),
-    # path('users/', include('users.api.v1.urls')),
+    path('users/', include('apps.users.api.v1.urls')),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('ai/', include('apps.ai_engine.api.v1.urls')),
+    # path('tenders/', include('apps.tenders.api.v1.urls')),
 ]
